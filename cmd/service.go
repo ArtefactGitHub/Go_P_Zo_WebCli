5f12f9982396e2f032950ec12f642ded4ffdefdf
ArtefactGitHub <artefactgo@gmail.com>
2022-02-11 10:33:46 +0900

    Zo投稿処理の追加

--- cmd/service.go
+++ cmd/service.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
 
 type service struct {
 }
@@ -9,7 +15,7 @@ func NewService() *service {
 	return &service{}
 }
 
-func (s *service) GetMypageViewModel(userToken *UserToken) (*MypageGetModel, error) {
+func (s *service) GetMypage(userToken *UserToken) (*MypageGetModel, error) {
 	resUser, err := RequestGetUser(userToken)
 	if err != nil || resUser.StatusCode != http.StatusOK {
 		return nil, err
@@ -22,4 +28,36 @@ func (s *service) GetMypageViewModel(userToken *UserToken) (*MypageGetModel, err
 
 	result := NewMypageGetModel(resUser.FamilyName+resUser.GivenName, resUser.Email, *resZo.Zos, resZo.ResponseBase)
 	return result, err
+}
+
+func (s *service) PostNewZo(userToken *UserToken, values url.Values) (*MypageGetModel, error) {
+	rz, err := convertRequestZo(values.Get("achievementdate"), values.Get("exp"), values.Get("categoryId"), values.Get("content"))
+	if err != nil {
+		return nil, err
+	}
+
+	resZo, err := RequestPostZo(userToken, rz)
+	if err != nil || resZo.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+func convertRequestZo(_achievementDate string, _exp string, _categoryId string, message string,
+) (*requestZo, error) {
+	if achievementDate, err := time.Parse(TimeLayout, _achievementDate); err != nil {
+		return nil, fmt.Errorf("達成日が正しい値ではありません。achievementDate: %s", _achievementDate)
+	} else if exp, err := strconv.Atoi(_exp); err != nil {
+		return nil, fmt.Errorf("獲得経験値が正しい値ではありません。exp: %s", _exp)
+	} else if categoryId, err := strconv.Atoi(_categoryId); err != nil {
+		return nil, fmt.Errorf("カテゴリーIDが正しい値ではありません。categoryId: %s", _categoryId)
+	} else {
+		return NewRequestZo(
+			achievementDate,
+			exp,
+			categoryId,
+			message,
+		), nil
+	}
 }
